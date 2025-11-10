6207e8268e7e636986ed639f281e34d5a2351d52
francis <FrancisMingomba>
2025-11-10 01:19:34 +0000

    WIP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { Registerform } from './registerform/registerform';
 import { ChangePassword } from './change-password/change-password';
 import { AuthReset } from './auth-reset/auth-reset';
 import { Home } from './home/home';
-import { FormExample } from './form-example/form-example';
+
 
 
 export const routes: Routes = [
@@ -17,6 +17,5 @@ export const routes: Routes = [
     {path: 'changepassword' , component:ChangePassword},
     {path: 'authreset' , component:AuthReset},
     {path: 'home' , component:Home},
-    {path: 'formExample' , component:FormExample},
     {path: '**' , component:PageNotFound}
 ];
